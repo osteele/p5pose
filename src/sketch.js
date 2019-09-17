5fd5633e9fc1464027502989b6ed177f6b41c83a
Oliver Steele <steele@osteele.com>
2019-09-17 18:29:43 +0800

    Read poses from opttrack server

--- src/sketch.js
+++ src/sketch.js
@@ -19,6 +19,97 @@ export function setSketch(sketch) {
   p5 = sketch;
 }
 
+function makeSkeletonPairs() {
+  const pairs = [];
+  // TODO: LHand HeadSide LWristIn WaistLBackFront
+  [
+    'LToe LFoot LAnkleOut LKneeOut LShin LThigh Hip',
+    'LFArm LWristOut LHandOut LWristIn',
+    'LHand LWristIn',
+  ].forEach((sequence) => {
+    let prev = null;
+    sequence.split(' ').forEach((name) => {
+      if (prev) {
+        pairs.push([prev, name]);
+        if (prev[0] == 'L' && name[0] == 'L') {
+          pairs.push(['R' + prev.slice(1), 'R' + name.slice(1)]);
+        } else if (prev[0] == 'L') {
+          pairs.push(['R' + prev.slice(1), name]);
+        } else if (name[0] == 'L') {
+          pairs.push([prev, 'R' + name.slice(1)]);
+        }
+      }
+      prev = name;
+    });
+  });
+  return pairs;
+}
+
+const skeletonPairs = makeSkeletonPairs();
+// console.info(skeletonPairs);
+
+function optitrack(elt, onLoad) {
+  const width = elt.width;
+  const height = elt.height;
+  const ws = new WebSocket('ws://localhost:8765');
+  const callbacks = [];
+  const elapsedTimeScale = 5;
+
+  const startTime = new Date();
+  let cursor = 0;
+
+  ws.onmessage = (event) => {
+    const pose = JSON.parse(event.data);
+    const byPart = {};
+    pose.keypoints.forEach((kp) => {
+      byPart[kp.part] = kp;
+      const pos = kp.position;
+      kp.pos = pos; // this is 3D position
+      // create new 2D position
+      kp.position = {
+        x: (1 + pos.x) * width / 2,
+        y: (1 - pos.z) * height / 2,
+      };
+    });
+    const skeleton = [];
+    skeletonPairs.forEach((names) => {
+      const p1 = byPart[names[0]];
+      const p2 = byPart[names[1]];
+      if (p1 && p2) {
+        skeleton.push([p1, p2]);
+      }
+    });
+    let poses = [{ pose, skeleton }];
+    callbacks.forEach((cb) => {
+      p5.push();
+      try {
+        cb(poses);
+      } finally {
+        p5.pop();
+      }
+    });
+  };
+
+  function requestData() {
+    const elapsed = new Date() - startTime;
+    if (ws.readyState == 1) {
+      if (onLoad) {
+        onLoad();
+        onLoad = null;
+      }
+      ws.send(JSON.stringify({ elapsed, cursor }));
+      cursor += elapsedTimeScale;
+    }
+    requestAnimationFrame(requestData);
+  }
+  requestAnimationFrame(requestData);
+  // setInterval(requestData, 10);
+
+  return {
+    on: (eventType, cb) => callbacks.push(cb),
+  };
+}
+
 // p5js calls this code once when the page is loaded (and, during
 // development, when the code is modified.)
 export function setup() {
@@ -29,7 +120,8 @@ export function setup() {
   // Create a new poseNet method with single-pose detection.
   // The second argument is a function that is called when the model is
   // loaded. It hides the HTML element that displays the "Loading model…" text.
-  const poseNet = ml5.poseNet(video, () => p5.select('#status').hide());
+  // const poseNet = ml5.poseNet(video, () => p5.select('#status').hide());
+  const poseNet = optitrack(video, () => p5.select('#status').hide());
 
   // Every time we get a new pose, apply the function `drawPoses` to it
   // (call `drawPoses(poses)`) to draw it.
@@ -43,7 +135,7 @@ export function setup() {
 // does nothing---instead, the call to `poseNet.on` in `setup` (above) specifies
 // a function that is applied to the list of poses whenever PoseNet processes
 // a video frame.
-export function draw() {}
+export function draw() { }
 
 function drawPoses(poses) {
   // Modify the graphics context to flip all remaining drawing horizontally.
@@ -51,7 +143,8 @@ function drawPoses(poses) {
   // is easier to work with.
   p5.translate(width, 0); // move the left side of the image to the right
   p5.scale(-1.0, 1.0);
-  p5.image(video, 0, 0, video.width, video.height);
+  // p5.image(video, 0, 0, video.width, video.height);
+  p5.rect(0, 0, video.width, video.height);
   drawKeypoints(poses);
   drawSkeleton(poses);
 }
