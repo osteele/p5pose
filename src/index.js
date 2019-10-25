455b4f25dd5d667c45b4f1f0d1848d282855716f
Oliver Steele <steele@osteele.com>
2019-10-25 15:03:35 +0800

    Use p5 in global mode

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import * as sketch from './sketch';
 
-// Force page refresh on hot reload
-if (module.hot) {
-  module.hot.accept(() => window.location.reload());
-}
-
-const s2 = (p5s) => {
-  sketch.setSketch(p5s);
-  p5s.setup = sketch.setup;
-  p5s.draw = sketch.draw;
-};
-
-// eslint-disable-next-line new-cap
-new p5(s2, 'container');
+Object.keys(sketch).forEach((key) =>
+  global[key] = sketch[key]);
--- src/sketch.js
+++ src/sketch.js
@@ -1,35 +1,17 @@
-const scale = 1; // scale the video image
-
-// video image dimensions
-const width = 640 * scale;
-const height = 480 * scale;
-
-// setSketch (below) sets this to a p5 instance.
-// In this file, the p5.js API functions are accessible as methods of this
-// instance.
-// See https://github.com/processing/p5.js/wiki/Global-and-instance-mode
-let p5;
-
 // setup initializes this to a p5.js Video instance.
 let video;
-
-// index.js calls this to set p5 to the current p5 sketch instance, so that
-// setup and draw can access it.
-export function setSketch(sketch) {
-  p5 = sketch;
-}
 
 // p5js calls this code once when the page is loaded (and, during
 // development, when the code is modified.)
 export function setup() {
-  p5.createCanvas(width, height);
-  video = p5.select('video') || p5.createCapture(p5.VIDEO);
+  createCanvas(640, 480);
+  video = select('video') || createCapture(VIDEO);
   video.size(width, height);
 
   // Create a new poseNet method with single-pose detection.
   // The second argument is a function that is called when the model is
   // loaded. It hides the HTML element that displays the "Loading model…" text.
-  const poseNet = ml5.poseNet(video, () => p5.select('#status').hide());
+  const poseNet = ml5.poseNet(video, () => select('#status').hide());
 
   // Every time we get a new pose, apply the function `drawPoses` to it
   // (call `drawPoses(poses)`) to draw it.
@@ -43,15 +25,15 @@ export function setup() {
 // does nothing---instead, the call to `poseNet.on` in `setup` (above) specifies
 // a function that is applied to the list of poses whenever PoseNet processes
 // a video frame.
-export function draw() {}
+export function draw() { }
 
 function drawPoses(poses) {
   // Modify the graphics context to flip all remaining drawing horizontally.
   // This makes the image act like a mirror (reversing left and right); this
   // is easier to work with.
-  p5.translate(width, 0); // move the left side of the image to the right
-  p5.scale(-1.0, 1.0);
-  p5.image(video, 0, 0, video.width, video.height);
+  translate(width, 0); // move the left side of the image to the right
+  scale(-1.0, 1.0);
+  image(video, 0, 0, video.width, video.height);
   drawKeypoints(poses);
   drawSkeleton(poses);
 }
@@ -61,9 +43,9 @@ function drawKeypoints(poses) {
   poses.forEach((pose) =>
     pose.pose.keypoints.forEach((keypoint) => {
       if (keypoint.score > 0.2) {
-        p5.fill(0, 255, 0);
-        p5.noStroke();
-        p5.ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        fill(0, 255, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
     })
   );
@@ -75,8 +57,8 @@ function drawSkeleton(poses) {
     pose.skeleton.forEach((skeleton) => {
       // skeleton is an array of two keypoints. Extract the keypoints.
       const [p1, p2] = skeleton;
-      p5.stroke(255, 0, 0);
-      p5.line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
+      stroke(255, 0, 0);
+      line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
     });
   });
 }
