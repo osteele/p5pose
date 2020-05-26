5cda2d461166a13b21b8f9090e84ac2bd090ef9d
Oliver Steele <steele@osteele.com>
2020-05-26 14:54:42 -0400

    Simplify; move drawing to draw function

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-// setup initializes this to a p5.js Video instance.
-let video;
+let video; // setup initializes this to a p5.js Video instance.
+let poses = []; // the poseNet.on callback sets this from new poses
 
 // p5js calls this code once when the page is loaded (and, during development,
 // when the code is modified.)
@@ -11,23 +11,17 @@ export function setup() {
   // Create a new poseNet method with single-pose detection. The second argument
   // is a function that is called when the model is loaded. It hides the HTML
   // element that displays the "Loading model…" text.
-  const poseNet = ml5.poseNet(video, () => select("#status").hide());
+  const poseNet = ml5.poseNet(video, () => { select("#status").hide() });
 
-  // Every time we get a new pose, apply the function `drawPoses` to it (call
-  // `drawPoses(poses)`) to draw it.
-  poseNet.on("pose", drawPoses);
+  // Every time we get a new set of poses, store them in the global variable `poses` so that .
+  poseNet.on("pose", newPoses => { poses = newPoses });
 
   // Hide the video element, and just show the canvas
   video.hide();
 }
 
-// p5js calls this function once per animation frame. In this program, it does
-// nothing---instead, the call to `poseNet.on` in `setup` (above) specifies a
-// function that is applied to the list of poses whenever PoseNet processes a
-// video frame.
-export function draw() {}
-
-function drawPoses(poses) {
+// p5js calls this function once per animation frame.
+export function draw() {
   // Modify the graphics context to flip all remaining drawing horizontally.
   // This makes the image act like a mirror (reversing left and right); this is
   // easier to work with.
@@ -40,25 +34,27 @@ function drawPoses(poses) {
 
 // Draw ellipses over the detected keypoints
 function drawKeypoints(poses) {
-  poses.forEach(pose =>
-    pose.pose.keypoints.forEach(keypoint => {
+  for (let pose of poses) {
+    for (let keypoint of pose.pose.keypoints) {
       if (keypoint.score > 0.2) {
         fill(0, 255, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
       }
-    })
-  );
+    }
+  }
 }
 
 // Draw connections between the skeleton joints.
 function drawSkeleton(poses) {
-  poses.forEach(pose => {
-    pose.skeleton.forEach(skeleton => {
-      // skeleton is an array of two keypoints. Extract the keypoints.
+  for (let pose of poses) {
+    for (let skeleton of pose.skeleton) {
+      // skeleton is an array of two keypoints. Extract the keypoints. This
+      // syntax means set p1 to the first element of the array that is the value
+      // of skeleton, and setting p2 to the second element.
       const [p1, p2] = skeleton;
       stroke(255, 0, 0);
       line(p1.position.x, p1.position.y, p2.position.x, p2.position.y);
-    });
-  });
+    }
+  }
 }
